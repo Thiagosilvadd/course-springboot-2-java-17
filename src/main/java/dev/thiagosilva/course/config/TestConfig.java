d582846c7123acecec0fe572247153281b398b1e
Thiago Silva <silvaa.thiagod2@gmail.com>
2022-07-08 09:36:56 -0300

    Producs

--- src/main/java/dev/thiagosilva/course/config/TestConfig.java
+++ src/main/java/dev/thiagosilva/course/config/TestConfig.java
@@ -2,10 +2,12 @@ package dev.thiagosilva.course.config;
 
 import dev.thiagosilva.course.entities.Category;
 import dev.thiagosilva.course.entities.Order;
+import dev.thiagosilva.course.entities.Product;
 import dev.thiagosilva.course.entities.User;
 import dev.thiagosilva.course.entities.enums.OrderStatus;
 import dev.thiagosilva.course.repositories.CategoryRepository;
 import dev.thiagosilva.course.repositories.OrderRepository;
+import dev.thiagosilva.course.repositories.ProductRepository;
 import dev.thiagosilva.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,6 +30,8 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -36,7 +40,14 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+//
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3,p4,p5));
 
 
 
---
+++ src/main/java/dev/thiagosilva/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package dev.thiagosilva.course.repositories;
+
+import dev.thiagosilva.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
