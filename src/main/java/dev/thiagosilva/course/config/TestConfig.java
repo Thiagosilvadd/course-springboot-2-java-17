ff4f6936e71be80d772d97f8081bd019365072f4
Thiago Silva <silvaa.thiagod2@gmail.com>
2022-07-09 14:11:25 -0300

    Product-OrderItem one-to-many association

--- src/main/java/dev/thiagosilva/course/config/TestConfig.java
+++ src/main/java/dev/thiagosilva/course/config/TestConfig.java
@@ -1,14 +1,8 @@
 package dev.thiagosilva.course.config;
 
-import dev.thiagosilva.course.entities.Category;
-import dev.thiagosilva.course.entities.Order;
-import dev.thiagosilva.course.entities.Product;
-import dev.thiagosilva.course.entities.User;
+import dev.thiagosilva.course.entities.*;
 import dev.thiagosilva.course.entities.enums.OrderStatus;
-import dev.thiagosilva.course.repositories.CategoryRepository;
-import dev.thiagosilva.course.repositories.OrderRepository;
-import dev.thiagosilva.course.repositories.ProductRepository;
-import dev.thiagosilva.course.repositories.UserRepository;
+import dev.thiagosilva.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -67,6 +64,13 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
     }
 }
---
+++ src/main/java/dev/thiagosilva/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package dev.thiagosilva.course.repositories;
+
+import dev.thiagosilva.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
